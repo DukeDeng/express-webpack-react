1b4a8bd9a807a14687ee97faff5c12d64a476543
everseeker0307 <everseeker0307@gmail.com>
2016-09-20 15:32:44 +0800

    initial

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var compression = require('compression');
 var webpack = require('webpack');
 var webpackMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
@@ -8,13 +9,14 @@ var config = require('./webpack.config.js');
 var app = express();
 var compiler = webpack(config);
 
-app.use(express.static('dist'));
-
+app.use(compression());
 app.use(webpackMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
+
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
-var path = require('path');
 var webpack = require('webpack');
 var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+var nodeModulesPath = path.join(__dirname, '/node_modules');
 
 var config = {
   devtool: 'eval-source-map',
-  entry: {
-    index: [
-      hotMiddlewareScript,
-      path.join(__dirname, 'src/components/index.jsx')
-    ]
-  },
   resolve: {
     //require文件的时候不需要写后缀了，可以自动补全
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
+  },
+  alias: {
+    'react': path.join(nodeModulesPath, '/react/dist/react.min'),
+    'react-dom': path.join(nodeModulesPath, '/react-dom/dist/react-dom.min')
+  },
+  entry: {
+    index: [
+      'webpack/hot/dev-server',
+      hotMiddlewareScript,
+      path.join(__dirname, 'src/components/index.jsx')
+    ],
+    common: ['react', 'react-dom']
   },
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
     publicPath: 'http://localhost:3000/'
+  },
+  externals: {
+    'jquery': 'jQuery',
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    // 'react-router': 'ReactRouter',
+    // 'antd': 'antd'
   },
   module: {
     loaders: [{
@@ -43,10 +58,22 @@ var config = {
     }]]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
     new webpack.optimize.OccurenceOrderPlugin(),    //根据模块调用次数，给模块分配ids，常被调用的ids分配更短的id，使得ids可预测，降低文件大小
     new webpack.HotModuleReplacementPlugin(),       //如果代码更改，自动刷新页面
     new webpack.NoErrorsPlugin(),                   //报错但不退出webpack进程
+    new CopyWebpackPlugin([
+      { from: 'src/static/react.min.js' },
+      { from: 'src/static/react-dom.min.js' },
+      { from: 'src/static/jquery.min.js' }
+    ]),
+    //压缩打包的文件
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        //supresses warnings, usually from module minification
+        warnings: false
+      }
+    })
   ]
 };
 
